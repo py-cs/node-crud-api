445b587a4dbfe68fa835e2fe3d32d0555d95a7d7
py-cs <e.1@mail.ru>
2022-12-06 00:49:33 +0300

    refactor: transfer validation logic from controller into service

--- src/users/controller.ts
+++ src/users/controller.ts
@@ -1,10 +1,6 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { userService } from "./service";
-import { getBody, getId, isUser } from "./utils";
-import { validate } from "uuid";
-import { User } from "./types";
-import ApiError from "../apiError/apiError";
-import { ErrorMessages } from "../apiError/constants";
+import { getBody, getId } from "./utils";
 
 export enum HTTPCodes {
   OK = 200,
@@ -14,60 +10,6 @@ export enum HTTPCodes {
   NOT_FOUND = 404,
 }
 
-export const userController = {
-  async getAll(_: IncomingMessage, res: ServerResponse<IncomingMessage>) {
-    const users = await userService.getAll();
-    sendResponse<User[]>(res, users);
-  },
-
-  async getOne(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
-    const id = getId(req.url);
-
-    if (!validate(id)) {
-      throw ApiError.badRequest(ErrorMessages.INVALID_ID);
-    }
-
-    const user = await userService.getOne(id);
-    sendResponse(res, user);
-  },
-
-  async create(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
-    const body = await getBody(req);
-
-    if (!isUser(body)) {
-      throw ApiError.badRequest(ErrorMessages.INVALID_DATA);
-    }
-    const newUser = await userService.create(body);
-    sendResponse(res, newUser, HTTPCodes.CREATED);
-  },
-
-  async delete(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
-    const id = getId(req.url);
-
-    if (!validate(id)) {
-      throw ApiError.badRequest(ErrorMessages.INVALID_ID);
-    }
-
-    const isDeleted = await userService.delete(id);
-    sendResponse(res, isDeleted, HTTPCodes.NO_CONTENT);
-  },
-
-  async update(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
-    const id = getId(req.url);
-    const body = await getBody(req);
-
-    if (!validate(id)) {
-      throw ApiError.badRequest(ErrorMessages.INVALID_ID);
-    }
-    if (!isUser(body)) {
-      throw ApiError.badRequest(ErrorMessages.INVALID_DATA);
-    }
-
-    const updatedUser = await userService.update(id, body);
-    sendResponse(res, updatedUser);
-  },
-};
-
 const sendResponse = <T>(
   res: ServerResponse<IncomingMessage>,
   data: T,
@@ -75,4 +17,36 @@ const sendResponse = <T>(
 ) => {
   res.statusCode = status;
   res.end(JSON.stringify(data));
+};
+
+export const userController = {
+  async getAll(_: IncomingMessage, res: ServerResponse<IncomingMessage>) {
+    const users = await userService.getAll();
+    sendResponse(res, users);
+  },
+
+  async getOne(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
+    const id = getId(req.url);
+    const user = await userService.getOne(id);
+    sendResponse(res, user);
+  },
+
+  async create(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
+    const body = await getBody(req);
+    const newUser = await userService.create(body);
+    sendResponse(res, newUser, HTTPCodes.CREATED);
+  },
+
+  async delete(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
+    const id = getId(req.url);
+    const isDeleted = await userService.delete(id);
+    sendResponse(res, isDeleted, HTTPCodes.NO_CONTENT);
+  },
+
+  async update(req: IncomingMessage, res: ServerResponse<IncomingMessage>) {
+    const id = getId(req.url);
+    const body = await getBody(req);
+    const updatedUser = await userService.update(id, body);
+    sendResponse(res, updatedUser);
+  },
 };
--- src/users/service.ts
+++ src/users/service.ts
@@ -1,5 +1,8 @@
+import { validate } from "uuid";
+import ApiError from "../apiError/apiError";
 import { userRepository } from "./repository";
-import { User } from "./types";
+import { ErrorMessages } from "../apiError/constants";
+import { isUser } from "./utils";
 
 export const userService = {
   async getAll() {
@@ -7,18 +10,33 @@ export const userService = {
   },
 
   async getOne(id: string) {
+    if (!validate(id)) {
+      throw ApiError.badRequest(ErrorMessages.INVALID_ID);
+    }
     return userRepository.getOne(id);
   },
 
-  async create(user: User) {
+  async create(user: unknown) {
+    if (!isUser(user)) {
+      throw ApiError.badRequest(ErrorMessages.INVALID_DATA);
+    }
     return userRepository.create(user);
   },
 
   async delete(id: string) {
+    if (!validate(id)) {
+      throw ApiError.badRequest(ErrorMessages.INVALID_ID);
+    }
     return userRepository.delete(id);
   },
 
-  async update(id: string, user: User) {
+  async update(id: string, user: unknown) {
+    if (!validate(id)) {
+      throw ApiError.badRequest(ErrorMessages.INVALID_ID);
+    }
+    if (!isUser(user)) {
+      throw ApiError.badRequest(ErrorMessages.INVALID_DATA);
+    }
     return userRepository.update(id, user);
   },
 };
